3d66379a6db604e934eb79a72f23d41d41264e00
neulhan <sting7675@naver.com>
2020-04-12 17:08:03 +0900

    a

--- config/settings.py
+++ config/settings.py
@@ -119,5 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "collectstatic")
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "dev_static"),)  # 개발 단계에서 사용하는 static 경로
--- scripts/afterInstall.py
+++ scripts/afterInstall.py
@@ -9,4 +9,6 @@ os.system('python3 -m venv /home/ubuntu/djangoAutoDeployTutorial/venv')
 os.system('sudo chown -R ubuntu.ubuntu /home/ubuntu/djangoAutoDeployTutorial')
 os.system('sudo chmod -R gu+rwx /home/ubuntu/djangoAutoDeployTutorial')
 os.system(
-    '. /home/ubuntu/djangoAutoDeployTutorial/venv/bin/activate && pip install --upgrade pip && pip install -r /home/ubuntu/djangoAutoDeployTutorial/requirements.txt && python /home/ubuntu/djangoAutoDeployTutorial/manage.py migrate')
+    '. /home/ubuntu/djangoAutoDeployTutorial/venv/bin/activate && pip3 install --upgrade pip && pip3 install -r /home/ubuntu/djangoAutoDeployTutorial/requirements.txt && python3 /home/ubuntu/djangoAutoDeployTutorial/manage.py collectstatic --noinput  && python3 /home/ubuntu/djangoAutoDeployTutorial/manage.py migrate')
+os.system("sudo service uwsgi restart")
+os.system("sudo service nginx restart")
